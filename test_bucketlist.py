b41c9297f604d276037ec52afb5cfd55f66fdef0
Anillab <anillabusolo@gmail.com>
2018-11-28 21:08:20 +0300

    defined a setup func tahat initializes app and test variables

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -3,6 +3,17 @@ import os
 import json
 from app import create_app,db
 
+class Test_Case_Bucketlist(unittest.TestCase):
+
+    def SetUp(self):
+        self.app=create_app(config_name='testing')
+        self.client=self.app.test_client
+        self.bucketlist={'name':'Take a road trip to SouthAfrica'}
+
+        # binds the app to the current context
+        with self.app_context():
+            db.create_all()
+
     def tearDown(self):
         '''
         tear down all initialized variables
