d644219d68ac7155104efa359ccb9acad60290fe
Anillab <anillabusolo@gmail.com>
2018-12-17 23:43:27 +0300

    added the server command

--- manage.py
+++ manage.py
@@ -8,10 +8,8 @@ manager=Manager(app)
 migrate=Migrate(app,db)
 
 manager.add_command('db',MigrateCommand)
-# manager.add_command('server',Server)
-# @manager.shell
-# def make_shell_context():
-#     return dict(app=app,db=db)
+manager.add_command('server',Server)
+
 
 if __name__ == '__main__':
     manager.run()
